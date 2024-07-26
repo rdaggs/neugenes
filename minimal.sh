9ca0be38ecc54eb69432d4ced29c89fbc8158ec5
rdaggs <135763470+rdaggs@users.noreply.github.com>
2024-07-26 14:37:50 -0700

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-#python3.11 -m venv venv
+python3.11 -m venv venv
 source venv/bin/activate
 pushd model
-#pip install -r requirements.txt
+pip install -r requirements.txt
 python -u main.py --dir test --structures 'BLA,PVT,MH,CA,'
 popd
 t
